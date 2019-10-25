bffb4a4c1f69955fd27fe47bccc7f03506cc6a55
Devraj Mehta <devm33@gmail.com>
2019-10-25 12:33:08 -0400

    create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent],
 })
