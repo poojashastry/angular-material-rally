b5ce0732998cfcfb6316993eb424cd1626de41f8
Pooja Shastry <poojashastry20@gmail.com>
2019-10-25 14:40:40 -0700

    Add styles to finance rally card

--- src/app/rally-finance-card/rally-finance-card.module.ts
+++ src/app/rally-finance-card/rally-finance-card.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { RallyFinanceCardComponent } from './rally-finance-card.component';
 
 @NgModule({
   declarations: [RallyFinanceCardComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   exports: [RallyFinanceCardComponent],
 })
 export class RallyFinanceCardModule {}
