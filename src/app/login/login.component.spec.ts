47085a006f3ed2a4ea1349d2884596415ea2e05a
Jamie Arseneault <jamie@jamiearseneault.com>
2019-10-25 17:07:54 -0400

    Add tests for login component

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -39,4 +39,32 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show a checkbox in username if populated', () => {
+    component.username = 'username';
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('.username-icon')).toBeTruthy();
+  });
+
+  it('should not show a checkbox in username if not populated', () => {
+    component.username = '';
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('.username-icon')).toBeFalsy();
+  });
+
+  it('should show a checkbox in password if >= than expected chars', () => {
+    component.password = '123456789';
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('.password-icon')).toBeTruthy();
+  });
+
+  it('should not show a checkbox in password if < than expected chars', () => {
+    component.password = '123';
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('.password-icon')).toBeFalsy();
+  });
 });
