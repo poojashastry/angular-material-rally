97d67695ccf5077f462d3175842183c7dcc44491
Justin Ying <just10ying@gmail.com>
2019-10-25 11:50:31 -0700

    Add flex styles for finance card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rally';
+  accountItems = [
+    { title: 'Checking', subtitle: '********1234', value: 2215.13 },
+    { title: 'Home Savings', subtitle: '********4234', value: 8676.88 },
+    { title: 'Car savings', subtitle: '********9012', value: 900.13 },
+  ];
 }
