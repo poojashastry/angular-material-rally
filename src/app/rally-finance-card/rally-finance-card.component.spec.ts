a7133a76a01268e2406b388d4d5e0c121d9cdccd
Bohao Wang <bohao@google.com>
2019-11-25 15:59:30 -0800

    fix test

--- src/app/rally-finance-card/rally-finance-card.component.spec.ts
+++ src/app/rally-finance-card/rally-finance-card.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RallyFinanceCardComponent } from './rally-finance-card.component';
 import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material';
 
 describe('RallyFinanceCardComponentComponent', () => {
   let component: RallyFinanceCardComponent;
@@ -9,7 +11,7 @@ describe('RallyFinanceCardComponentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatCardModule],
+      imports: [MatCardModule, FlexLayoutModule, MatIconModule],
       declarations: [RallyFinanceCardComponent],
     }).compileComponents();
   }));
