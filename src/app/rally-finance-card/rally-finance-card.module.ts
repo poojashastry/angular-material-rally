fae77c81e10db08fb68a70e5bd705bad4e7417b6
Bohao Wang <bohao@google.com>
2019-11-25 15:54:45 -0800

    add fxlayout and responsiveness to finance card

--- src/app/rally-finance-card/rally-finance-card.module.ts
+++ src/app/rally-finance-card/rally-finance-card.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { RallyFinanceCardComponent } from './rally-finance-card.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [RallyFinanceCardComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, FlexLayoutModule],
   exports: [RallyFinanceCardComponent],
 })
 export class RallyFinanceCardModule {}
