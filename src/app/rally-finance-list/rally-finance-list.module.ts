5ecef9acf2d9392438b488ab7a73e8b357195ec8
Bohao Wang <bohao@google.com>
2019-11-30 19:17:57 -0800

    Add tests and see all button

--- src/app/rally-finance-list/rally-finance-list.module.ts
+++ src/app/rally-finance-list/rally-finance-list.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { RallyFinanceListComponent } from './rally-finance-list.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from "@angular/material";
 
 @NgModule({
   declarations: [RallyFinanceListComponent],
-  imports: [CommonModule, MatIconModule, FlexLayoutModule],
+  imports: [CommonModule, MatIconModule, FlexLayoutModule, MatButtonModule],
   exports: [RallyFinanceListComponent],
 })
 export class RallyFinanceListModule {}
