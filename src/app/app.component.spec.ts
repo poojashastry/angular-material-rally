2f50b04d05ce35077ac7b238dafa67dc5a04928c
Joe Zischkau <906641+jzischkau@users.noreply.github.com>
2020-01-27 18:10:06 -0500

    Hide nav on login route

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, tick, fakeAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,12 +7,25 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RallyFinanceCardModule } from './rally-finance-card/rally-finance-card.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'fake-login',
+  template: '',
+  styleUrls: [],
+})
+class FakeComponent {}
 
 describe('AppComponent', () => {
+  let router: Router;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'login', component: FakeComponent },
+        ]),
         RallyFinanceCardModule,
         MatIconModule,
         MatSidenavModule,
@@ -20,8 +33,9 @@ describe('AppComponent', () => {
         MatToolbarModule,
         FlexLayoutModule,
       ],
-      declarations: [AppComponent],
+      declarations: [AppComponent, FakeComponent],
     }).compileComponents();
+    router = TestBed.get(Router);
   }));
 
   it('should create the app', () => {
@@ -35,4 +49,17 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('rally');
   });
+
+  it('should hide the toolbar and sidebar on the login route', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.ngZone.run(() => {
+      router.navigate(['login']);
+    });
+    tick();
+    expect(fixture.nativeElement.querySelector('mat-toolbar')).toBeFalsy();
+    expect(
+      fixture.debugElement.query(By.css('mat-sidenav')).componentInstance
+        .opened
+    ).toBe(false);
+  }));
 });
